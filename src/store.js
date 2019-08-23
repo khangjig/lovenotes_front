12dcad277bfa323c3aa65b9fd7cbb86931d82882
Khang Le <khangld.citynow@gmail.com>
2019-08-24 04:56:07 +0700

    update

--- src/store.js
+++ src/store.js
@@ -1,11 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { connectRouter } from 'connected-react-router'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/reducer'
 
 const store = createStore(
-  // connectRouter(rootReducer),
-  // compose(applyMiddleware(thunk))
   rootReducer,
   applyMiddleware(thunk)
 )
